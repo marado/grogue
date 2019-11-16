a957d6737792152c52f7ab8b89272bf2b33eff98
Marcos Marado <mindboosternoori@gmail.com>
2019-11-16 16:13:07 +0000

    The links now point to the https versions

--- config.js
+++ config.js
@@ -10,10 +10,10 @@ var N = 4;
 
 // this is where you configure the websites you want to show
 var sites = [
-		{alt: "Kokori",        image:"https://kokori.grogue.org/images/DSC_0781_edit-600.jpg",                                               link:"http://kokori.grogue.org"          },
-		{alt: "Qink",          image:"https://qink.grogue.org/teaser.png",                                                                   link:"http://qink.grogue.org"            },
-		{alt: "Merankorii",    image:"https://farm6.staticflickr.com/5155/7183716806_996dcdd71d.jpg",                                        link:"http://merankorii.grogue.org"      },
-		{alt: "Noori Records", image:"https://noorirecords.files.wordpress.com/2010/11/redsilk.jpeg?w=400",                                  link:"http://noorirecords.grogue.org"    },
-		{alt: "+ko+ko+",       image:"https://noorirecords.files.wordpress.com/2011/12/aptn.jpeg",                                           link:"http://pluskopluskoplus.grogue.org"},
-		{alt: "Selva Virtual", image:"selva.png",                                                                                            link:"http://selva.grogue.org"           },
+		{alt: "Kokori",        image:"https://kokori.grogue.org/images/DSC_0781_edit-600.jpg",              link:"https://kokori.grogue.org"          },
+		{alt: "Qink",          image:"https://qink.grogue.org/teaser.png",                                  link:"https://qink.grogue.org"            },
+		{alt: "Merankorii",    image:"https://farm6.staticflickr.com/5155/7183716806_996dcdd71d.jpg",       link:"https://merankorii.grogue.org"      },
+		{alt: "Noori Records", image:"https://noorirecords.files.wordpress.com/2010/11/redsilk.jpeg?w=400", link:"https://noorirecords.grogue.org"    },
+		{alt: "+ko+ko+",       image:"https://noorirecords.files.wordpress.com/2011/12/aptn.jpeg",          link:"https://pluskopluskoplus.grogue.org"},
+		{alt: "Selva Virtual", image:"selva.png",                                                           link:"https://selva.grogue.org"           },
 	];
