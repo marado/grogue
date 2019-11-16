9875112f6a3b25b0f8fa977fba2d22a0b9a871cf
Marcos Marado <mindboosternoori@gmail.com>
2019-11-16 16:09:31 +0000

    image links now in https

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ var N = 4;
 
 // this is where you configure the websites you want to show
 var sites = [
-		{alt: "Kokori",        image:"http://kokori.grogue.org/images/DSC_0781_edit-600.jpg",                                                       link:"http://kokori.grogue.org"          },
-		{alt: "Qink",          image:"http://qink.grogue.org/teaser.png",                                                                    link:"http://qink.grogue.org"            },
+		{alt: "Kokori",        image:"https://kokori.grogue.org/images/DSC_0781_edit-600.jpg",                                               link:"http://kokori.grogue.org"          },
+		{alt: "Qink",          image:"https://qink.grogue.org/teaser.png",                                                                   link:"http://qink.grogue.org"            },
 		{alt: "Merankorii",    image:"https://farm6.staticflickr.com/5155/7183716806_996dcdd71d.jpg",                                        link:"http://merankorii.grogue.org"      },
 		{alt: "Noori Records", image:"https://noorirecords.files.wordpress.com/2010/11/redsilk.jpeg?w=400",                                  link:"http://noorirecords.grogue.org"    },
 		{alt: "+ko+ko+",       image:"https://noorirecords.files.wordpress.com/2011/12/aptn.jpeg",                                           link:"http://pluskopluskoplus.grogue.org"},
